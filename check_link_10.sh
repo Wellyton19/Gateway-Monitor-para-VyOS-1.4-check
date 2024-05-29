6e579f94d21b42f39df8012c70e2f778c8e0a9eb
Wellyton19 <102568070+Wellyton19@users.noreply.github.com>
2024-05-29 17:18:11 -0400

    check_link_10.sh

--- check_link_10.sh
+++ check_link_10.sh
@@ -5,7 +5,7 @@
 # Funções log(), check_connectivity() e send_whatsapp_message() (sem alterações)
 
 # Nome do script principal (com caminho absoluto)
-script_name="/home/vyos/link_10"
+script_name="/home/vyos/link_10_com_whattapp"
 lock_file="/var/run/$script_name.lock"  # Arquivo de bloqueio
 
 # Verificar se o script principal está em execução e se o arquivo de bloqueio existe
